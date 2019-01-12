2d0545353af3501909429e89a9212d0d2841d342
djordjestanojevic <djordjestanojevic@icloud.com>
2019-01-12 16:59:24 +0000

    added screenshots

--- projekat.c
+++ projekat.c
@@ -675,9 +675,9 @@ void generateTexture(int i){
     
     glBindTexture(GL_TEXTURE_2D, names[i]);
     glTexParameteri(GL_TEXTURE_2D,
-                    GL_TEXTURE_WRAP_S, GL_CLAMP);
+                    GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D,
-                    GL_TEXTURE_WRAP_T, GL_CLAMP);
+                    GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D,
                     GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D,
